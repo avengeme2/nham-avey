e8925820ab56eef70d8aa84dfeeeb398bb4e83ea
Thenglong Heng <thenglongheng2k@gmail.com>
2022-08-13 07:20:10 +0700

    feat: init sentry module

---
+++ apps/api/src/sentry/sentry.providers.ts
@@ -0,0 +1,12 @@
+import { Provider } from "@nestjs/common"
+
+import { SENTRY_TOKEN } from "./sentry.constants"
+import { SentryModuleOptions } from "./sentry.interfaces"
+import { SentryService } from "./sentry.service"
+
+export function createSentryProviders(options: SentryModuleOptions): Provider {
+  return {
+    provide: SENTRY_TOKEN,
+    useValue: new SentryService(options),
+  }
+}
